6eb0e5d3f4a3ffaf1dea4d393223e33f56749000
Gybrán Pérez <gybranperez@gmail.com>
2022-02-19 14:03:58 -0600

    2 pcs

---
+++ src/main/java/mx/com/ciecas/app/BaseSpringBootReactorApplication.java
@@ -0,0 +1,46 @@
+package mx.com.ciecas.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import mx.com.ciecas.app.models.User;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class BaseSpringBootReactorApplication implements CommandLineRunner{
+
+	public static final Logger log = LoggerFactory.getLogger(BaseSpringBootReactorApplication.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BaseSpringBootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ejemploIterable();
+		
+	}
+	
+	public void ejemploFlatMap() throws Exception{
+		
+	}
+	
+	public void ejemploIterable() throws Exception{
+		
+		Flux<String> nombres = Flux.just("Gybran Perez", "Juan Perez", "Rodrigo Rosales", "Mario Casas", "Alfredo Jimenez", "Juan Hernandez");
+		
+		
+		nombres
+			.map(nombre -> new User(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
+			
+			.filter(usuario -> usuario.getNombre().equalsIgnoreCase("Juan"))
+			
+			.subscribe(
+					usuario -> log.info(usuario.toString())
+			);
+	}
+
+}
---
+++ src/main/java/mx/com/ciecas/app/models/User.java
@@ -0,0 +1,14 @@
+package mx.com.ciecas.app.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	private String nombre;
+	private String apellido;
+	
+}
